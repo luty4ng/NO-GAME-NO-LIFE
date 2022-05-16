4c9762429e3131e00d3f44afcfa240951fe45a57
RaymondWHZ <wuhaozhen1106@gmail.com>
2022-05-16 18:55:56 -0500

    Fix camera movement in Map 4 Inner

--- Assets/GameMain/Scripts/Map/Camera/Scene4CameraSwitch.cs
+++ Assets/GameMain/Scripts/Map/Camera/Scene4CameraSwitch.cs
@@ -6,9 +6,19 @@ public class Scene4CameraSwitch : MonoBehaviour
 {
     public CinemachineVirtualCamera wholeCamera;
 
+    private float switchStartTime = 0.0f;
+    private float startSwitchInterval = 1.0f;
+
+    private void Start()
+    {
+        switchStartTime = Time.time + startSwitchInterval;
+    }
+
     private void Update()
     {
-        if (!MapRegulator.current.DialogUIActive && Input.GetAxisRaw("Horizontal") != 0)  // if the player moves
+        if (!MapRegulator.current.DialogUIActive
+            && Time.time > switchStartTime
+            && Input.GetAxisRaw("Horizontal") != 0)  // if the player moves
         {
             wholeCamera.gameObject.SetActive(false);
         }
